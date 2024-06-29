d2719dea87f929dcc5fdf3b0b712a7111c0f4733
Arthur <atte.book@gmail.com>
2024-06-29 09:52:35 -0700

    tests: updated numpy backend test with new syntax

--- autodiff/tests/test_numpy_engine.py
+++ autodiff/tests/test_numpy_engine.py
@@ -2,33 +2,32 @@
 test the numpy backend
 """
 
-from autodiff import config, graph, llops
-from autodiff.engines.packaged_engines import numpy_engine
+from autodiff import config, llops, runtime
 
 
 def test_numpy_engine_add_reduce() -> None:
-    with config.Configuration(backend=numpy_engine.NUMPY_ENGINE):
-        x = graph.Symbol.load([1, 2, 3])
-        y = graph.Symbol.load([1, 2, 3])
-        z = x.do(llops.BinaryOps.MUL, y)  # 1, 4, 9
-        z_sum = z.do(llops.ReduceOps.SUM, axis=0)  # 14
+    with config.Configuration(engine=runtime.NumPyEngine()):
+        x = llops.ControlOps.LOAD([1, 2, 3])
+        y = llops.ControlOps.LOAD([1, 2, 3])
+        z = llops.BinaryOps.MUL(x, y)  # 1, 4, 9
+        z_sum = llops.ReduceOps.SUM(z, axis=0)  # 14
         res = z_sum.realize().to_python()
     assert res == 14
 
 
 def test_numpy_engine_assignment() -> None:
-    with config.Configuration(backend=numpy_engine.NUMPY_ENGINE):
-        x = graph.Symbol.load([1, 2, 3])
-        x = x.do(llops.MemOps.ASSIGN, graph.Symbol.load([4, 5, 6]))
+    with config.Configuration(engine=runtime.NumPyEngine()):
+        x = llops.ControlOps.LOAD([1, 2, 3])
+        x = llops.ControlOps.ASSIGN(x, llops.ControlOps.LOAD([4, 5, 6]))
         x_val = x.realize().to_python()
     assert x_val == [4, 5, 6]
 
 
 def test_numpy_engine_assign_full_loop() -> None:
-    with config.Configuration(backend=numpy_engine.NUMPY_ENGINE):
-        x = graph.Symbol.load([1, 2, 3])
-        x = x.do(llops.MemOps.ASSIGN, x.do(llops.BinaryOps.ADD, graph.Symbol.load([1, 1, 1])))
-        x = x.do(llops.MemOps.ASSIGN, x.do(llops.BinaryOps.ADD, graph.Symbol.load([1, 1, 1])))
+    with config.Configuration(engine=runtime.NumPyEngine()):
+        x = llops.ControlOps.LOAD([1, 2, 3])
+        x = llops.ControlOps.ASSIGN(x, llops.BinaryOps.ADD(x, llops.ControlOps.LOAD([1, 1, 1])))
+        x = llops.ControlOps.ASSIGN(x, llops.BinaryOps.ADD(x, llops.ControlOps.LOAD([1, 1, 1])))
         x_val = x.realize().to_python()
     assert x_val == [3, 4, 5]
 
