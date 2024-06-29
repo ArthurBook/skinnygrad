ebd51789c5404d5f3586e6cd719233d5bccd0ef1
Arthur <atte.book@gmail.com>
2024-06-29 09:52:14 -0700

    feat: stacked context configuration

--- autodiff/src/autodiff/__init__.py
+++ autodiff/src/autodiff/__init__.py
@@ -1,1 +1,11 @@
+from autodiff.config import Configuration
+from autodiff.runtime import Engine, NumPyEngine
+from autodiff.tensors import Tensor
 
+### Default configuration for the autodiff engine ###
+Configuration(
+    engine=NumPyEngine(),
+)
+
+
+__all__ = ["Engine", "NumPyEngine", "Configuration", "Tensor"]
--- autodiff/src/autodiff/config.py
+++ autodiff/src/autodiff/config.py
@@ -1,35 +1,58 @@
+"""
+# Global configuration for the autodiff package.
+
+User can modify this configuration in 2 ways:
+
+## Permanent change
+```python
+from autodiff import config
+config.Configuration(engine=engine)
+```
+
+## Temporary change
+```python
+from autodiff import config
+with config.Configuration(engine=engine):
+    ...
+"""
+
+from __future__ import annotations
+
+import collections
 import contextlib
-import threading
-from typing import ClassVar, TypeVar
+from typing import TYPE_CHECKING, Any, ClassVar, Type
 
-from autodiff import runtime
-from autodiff.engines.packaged_engines import numpy_engine
-
-GLOBAL_CTX_LOCK = threading.Lock()
+if TYPE_CHECKING:
+    from autodiff import runtime
 
 
-T = TypeVar("T")
+class _NOT_GIVEN: ...
 
 
-class UNSET: ...
+class _StackedConfigMeta(type):
+    __singletons__: ClassVar[dict[Type, Any]] = {}
+    __context_stack__: collections.ChainMap[str, Any]
+
+    def __call__(cls, **config_dict: Any) -> Configuration:
+        assert _NOT_GIVEN not in config_dict.values()
+        if cls in cls.__singletons__:  # update the singleton config context
+            cls.__context_stack__.maps.insert(0, config_dict)
+        else:  # initialize the singleton config and ctx stack that holds all attrs
+            cls.__context_stack__ = collections.ChainMap(config_dict)
+            cls.__setattribute__ = cls.__context_stack__.__setitem__
+            cls.__singletons__[cls] = super().__call__()
+        return cls.__singletons__[cls]
+
+    def __getattr__(cls: type[_StackedConfigMeta], key: str):
+        return cls.__context_stack__[key]
 
 
-class Configuration(contextlib.ContextDecorator):
-    backend: ClassVar[runtime.Engine]
+class Configuration(contextlib.ContextDecorator, metaclass=_StackedConfigMeta):
+    """Configuration for the autodiff package."""
 
-    def __init__(self, backend: runtime.Engine | UNSET = UNSET()) -> None:
-        Configuration.backend = backend  # type: ignore
+    engine: runtime.Engine
 
-    def __enter__(self):
-        return  ## TODO lock thread here in a way that GIL steps through this all the way first
-
-    def __exit__(self, a, b, c):
-        return
-
-
-###
-# Default config
-###
-Configuration(
-    backend=numpy_engine.NUMPY_ENGINE,
-)
+    def __init__(self, *, engine: runtime.Engine | _NOT_GIVEN = _NOT_GIVEN()) -> None: ...
+    def __enter__(self): ...
+    def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any) -> None:
+        self.__context_stack__.maps.pop(0)
